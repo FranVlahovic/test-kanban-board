5d3a83662fd79807c243504f381b45da3d5640d7
Fran Vlahovic <hovla.office@gmail.com>
2024-12-08 01:54:18 +0100

    Update HTML JS & CSS Updated HTML with Min Max length for Add Task Form. Stylized All Project Elements (Only minor changes left to be Added). Added Task Event Listeners Function to handle Move, Delete and Marking As Completed Buttons. Added Handle Delete Task. Added Task Counter (to keep count of Tasks in Container) Added Test Task.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./styles.css";
+import deleteIcon from "./assets/icons/delete.svg";
+import moveIcon from "./assets/icons/arrow-down-circle.svg";
+import profileIcon from "./assets/icons/account-circle.svg";
+
 
 const taskInput = document.querySelector('.task-name-input');
 const taskCategory = document.querySelector('.task-category-input');
@@ -51,6 +55,7 @@ const addTask = () => {
             closeTaskDialog();
             // saveTasksLocal(todoArray);
             renderTaskToDo();
+            taskCounter(todoArray);
         };
     });
 };
@@ -77,24 +82,65 @@ const addTaskInArray = (task) => {
 };
 
 const renderTaskToDo = () => {
-    const taskContainer = document.querySelector('.task-container');
-    taskContainer.innerHTML = '';
+    const todoContainer = document.getElementById('todo-container');
+    todoContainer.innerHTML = '';
 
     todoArray.forEach(({ taskId, taskName, taskCategory }) => {
-        taskContainer.innerHTML += `
+        todoContainer.innerHTML += `
         <div class="task-card" data-id="${taskId}">
             <h2 class="card-name">${taskName}</h2>
             <div class="card-buttons">
-                <button class="move-task-btn" data-id="${taskId}"><img src="assets/icons/arrow-down-circle.svg" alt="Down Arrow Icon"></button>
-                <button class="delete-task-btn" data-id="${taskId}"><img src="assets/icons/delete.svg" alt="Delete Icon"></button>
+                <button class="move-task-btn" data-id="${taskId}"><img src="${moveIcon}" alt="Down Arrow Icon"></button>
+                <button class="delete-task-btn" data-id="${taskId}"><img src="${deleteIcon}" alt="Delete Icon"></button>
             </div>
             <div class="card-category">${taskCategory}</div>
-            <img src="assets/icons/account-circle.svg" class="card-profile-image"></img>
+            <img src="${profileIcon}" class="card-profile-image"></img>
         </div>`;
     });
+    taskEventListeners();
+};
+
+const taskEventListeners = () => { 
+    const moveTaskBtns = document.querySelectorAll('.move-task-btn'); 
+    const deleteTaskBtns = document.querySelectorAll('.delete-task-btn'); 
+    const completeTaskBtns = document.querySelectorAll('.complete-task-btn'); 
+    
+    deleteTaskBtns.forEach(deleteTaskBtn => { 
+        deleteTaskBtn.addEventListener("click", (e) => {
+            const taskId = e.currentTarget.dataset.id; 
+            handleDeleteTask(taskId, todoArray); 
+        }); 
+    }); 
+}
+
+const handleDeleteTask = (taskId, array) => {
+    taskId = Number(taskId);
+
+    const taskIndex = array.findIndex(t => t.taskId === taskId);
+    if(taskIndex !== -1){
+        array.splice(taskIndex, 1);
+        renderTaskToDo();
+        taskCounter(todoArray);
+    }
+};
+
+
+const taskCounter = (array) => {
+    const taskNumber = array.length;
+    const taskDisplay = document.querySelector('.task-counter');
+
+    taskDisplay.textContent = taskNumber;
+};
+
+const testTask = () => {
+    const testTask = createTask("Test Task", "Test Category");
+    addTaskInArray(testTask);
+    renderTaskToDo();
+    taskCounter(todoArray);
 };
 
 //Innit
 openTaskDialogBtn();
 cancelAddTask();
 addTask();
+testTask();
